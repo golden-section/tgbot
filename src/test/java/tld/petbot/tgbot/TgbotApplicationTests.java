65665df6e9261d367dbee1c638fde8fa84564930
Alexander <a.gutsol@gmail.com>
2023-09-09 22:11:06 +0300

    First init

---
+++ src/test/java/tld/petbot/tgbot/TgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package tld.petbot.tgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
